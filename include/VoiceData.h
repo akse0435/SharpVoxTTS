8d86b4482d762b0d3667784038e36914b967566a
bytesizedfox <bytesizedfox@gmail.com>
2026-07-23 11:24:19 -0400

    tune default voice F4

--- include/VoiceData.h
+++ include/VoiceData.h
@@ -34,11 +34,11 @@ struct VoiceData {
     int16_t LipRounding = 0;     // -100=spread (F1 up,F2++,F3 up), 0=neutral, +100=rounded (F1 dn,F2--,F3 dn)
     int16_t OnsetHardness = 50;  // 0=soft breathy onset (slow ramp), 50=natural, 100=hard glottal attack (instant)
 
-    int16_t F4Freq = 3650;
+    int16_t F4Freq = 3825;
     int16_t F4BW = 200;
     int16_t F5Freq = 4500;
     int16_t F5BW = 250;
-    int16_t F4pFreq = 3650;
+    int16_t F4pFreq = 3825;
     int16_t F4pBW = 150;
     int16_t F5pFreq = 4200;
     int16_t F5pBW = 100;
